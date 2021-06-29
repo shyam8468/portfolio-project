a0ef73f791a58dd20b52dcc9b924f6978bd46064
Shyam Pande <shyampandey581995@gmail.com>
2021-06-29 16:49:53 +0530

    final

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+
+from . import views
+
+urlpatterns =[
+
+     path('',views.allblog , name='allblog'),
+     path('<int:blog_id>/',views.detail, name='detail'),
+
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+def allblog(request):
+    blogs = Blog.objects
+    return render(request,'blog/allblogs.html',{'blogs':blogs})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog,pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':detailblog})
